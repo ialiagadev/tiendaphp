06357edfb2309e3dcc69fa32f54851a364c4c06e
ivan <ialiagadev@gmail.com>
2025-01-26 22:42:31 +0100

    dbconfig

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 // Configuración de la base de datos
-define("DB_HOST", "localhost");              // Servidor MySQL proporcionado por Hostinger
-define("DB_NAME", "u669677445_tiendadb");    // Nombre de tu base de datos
-define("DB_USER", "u669677445_tiendadb");    // Usuario de la base de datos (parece que Hostinger usa el mismo nombre para el usuario y la base de datos)
-define("DB_PASS", "1PYagG[Ri=J7");           // Contraseña de la base de datos
-define("DB_CHARSET", "utf8mb4");             // Charset recomendado
+define("DB_HOST", "localhost");               // Host de la base de datos (parece que Hostinger usa "localhost")
+define("DB_NAME", "u669677445_tiendadb");     // Nombre de tu base de datos
+define("DB_USER", "u669677445_usuario_tienda"); // Usuario de la base de datos
+define("DB_PASS", "1PYagG[Ri=J7");            // Contraseña de la base de datos
+define("DB_CHARSET", "utf8mb4");              // Charset recomendado
 ?>
