661c6d5bbf6749c10aea73ae5832141c4125b978
ivan <ialiagadev@gmail.com>
2025-02-09 18:59:41 +0100

    db config

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 // Configuración de la base de datos
 define("DB_HOST", "localhost");               // Host de la base de datos (parece que Hostinger usa "localhost")
-define("DB_NAME", "u669677445_tiendadb");     // Nombre de tu base de datos
-define("DB_USER", "u669677445_usuario_tienda"); // Usuario de la base de datos
-define("DB_PASS", "1PYagG[Ri=J7");            // Contraseña de la base de datos
+define("DB_NAME", "u669677445_tienda_online");     // Nombre de tu base de datos
+define("DB_USER", "u669677445_ivan_aliaga"); // Usuario de la base de datos
+define("DB_PASS", "Practica_final0");            // Contraseña de la base de datos
 define("DB_CHARSET", "utf8mb4");              // Charset recomendado
 ?>
