982371ed6679ad5eda7a0452a42b00b969d33861
ivan <ialiagadev@gmail.com>
2025-02-09 20:44:51 +0100

    detalle producto

--- public/producto.php
+++ public/producto.php
@@ -1,11 +1,24 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once "../app/controllers/ProductoController.php";
 
 $controller = new ProductoController();
 
-if (isset($_GET['id'])) {
-    $controller->detalle($_GET['id']);
-} else {
-    $controller->index();
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    die("<div class='container mt-5 alert alert-danger'>Error: No se ha recibido el ID del producto.</div>");
 }
+
+$producto = $controller->obtenerProducto($_GET['id']);
+
+if (!$producto) {
+    die("<div class='container mt-5 alert alert-warning'>Error: Producto no encontrado en la base de datos.</div>");
+}
+
+// Comprobar si los datos del producto están bien cargados
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+exit();
 ?>
